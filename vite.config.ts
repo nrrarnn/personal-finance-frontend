12edb2cb87e26724ecd6d9659b189c191dbbed51
Nur Arini <nurarini0302@gmail.com>
2026-01-24 21:38:23 +0700

    chore: migrate to shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,5 +8,10 @@ export default defineConfig(() => {
     optimizeDeps: {
       include: ['chart.js', 'react-chartjs-2']
     },
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   }
 })
